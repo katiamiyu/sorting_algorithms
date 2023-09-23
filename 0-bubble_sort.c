658a5e5a32e398b853d0fc21be2ff7bf06ecf6c7
katiamiyu <katiamiyu@gmail.com>
2023-09-23 11:54:57 +0000

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,7 +13,7 @@ void bubble_sort(int *array, size_t size)
 	for (counter = 0; counter < size; counter++)
 	{
 		sorted = 1;
-		for (counter2 = 1; counter2 - counter < size; counter2++)
+		for (counter2 = 1; counter2 < size - counter; counter2++)
 		{
 			if (array[counter2] < array[counter2 - 1])
 			{
