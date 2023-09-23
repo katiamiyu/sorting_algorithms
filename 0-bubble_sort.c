9f091c8f5318b5b991edd0ed7034dd7febc6ac9f
katiamiyu <katiamiyu@gmail.com>
2023-09-23 11:29:39 +0000

    0-bubble_sort.c

---
+++ 0-bubble_sort.c
@@ -0,0 +1,26 @@
+#include "sort.h"
+/**
+ * bubble_sort - sorts a given array of integers.
+ * @array: array of integers.
+ * @size: total of array elements.
+ * Return: void.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t counter, counter2;
+	int temp;
+
+	for (counter = 0; counter < size; counter++)
+	{
+		for (counter2 = 1; counter2 < size; counter2++)
+		{
+			if (array[counter2] < array[counter2 - 1])
+			{
+				temp = array[counter2];
+				array[counter2] = array[counter2 - 1];
+				array[counter2 - 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
